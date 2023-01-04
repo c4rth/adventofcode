da0c28495299e5b728ebb4277bd69b26e57e7585
Thierry Carels <thierry.carels@gmail.com>
2023-01-04 16:28:32 +0100

    aoc2020 - day13

---
+++ src/test/kotlin/org/carth/aoc20/Day13Tests.kt
@@ -0,0 +1,22 @@
+package org.carth.aoc20
+
+import org.carth.common.DayTests
+import org.junit.jupiter.api.*
+
+class Day13Tests : DayTests<Day13>(Day13::class) {
+    @Test
+    @Order(1)
+    fun solvePartOneSample() = solve(Part.ONE, Type.TEST, expected = 295)
+
+    @Test
+    @Order(2)
+    fun solvePartOne() = solve(Part.ONE, Type.INPUT, expected = 3269)
+
+    @Test
+    @Order(3)
+    fun solvePartTwoSample() = solve(Part.TWO, Type.TEST, expected = 1068781L)
+
+    @Test
+    @Order(4)
+    fun solvePartTwo() = solve(Part.TWO, Type.INPUT, expected = 672754131923874L)
+}
