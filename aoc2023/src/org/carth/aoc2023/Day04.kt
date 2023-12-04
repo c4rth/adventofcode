c7fdac6ace802c39c21711f375800eeb01f040e2
Thierry Carels <thierry.carels@gmail.com>
2023-12-04 19:51:39 +0100

    Day04

--- aoc2023/src/org/carth/aoc2023/Day04.kt
+++ aoc2023/src/org/carth/aoc2023/Day04.kt
@@ -1,16 +1,42 @@
 package org.carth.aoc2023
 
 import org.carth.common.Puzzle
+import kotlin.math.pow
 
 class Day04(input: String) : Puzzle<Int, Int>() {
 
-    private val data = input.split(System.lineSeparator())
+    private fun parseNumbers(str: String) =
+        str.replace("  ", " ").trim().split(" ").map { it.toInt() }.toSet()
+
+    private val cards = input.split(System.lineSeparator()).map { line ->
+        line.split("Card ", ": ", " | ").let { (_, idx, w, n) ->
+            Card(idx.trim().toInt(), parseNumbers(w), parseNumbers(n))
+        }
+    }
 
     override fun solvePartOne(): Int {
-        return 1
+        return cards.sumOf { card ->
+            val matching = card.getMatchingNumber()
+            if (matching > 0) {
+                2.0.pow(matching - 1).toInt()
+            } else {
+                0
+            }
+        }
     }
 
     override fun solvePartTwo(): Int {
-        return 10
+        val wonCards = Array(cards.size) { 1 }
+        for (card in cards) {
+            val matching = card.getMatchingNumber()
+            (card.index..<card.index + matching).forEach { i ->
+                wonCards[i] += wonCards[card.index - 1]
+            }
+        }
+        return wonCards.sum()
+    }
+
+    class Card(val index: Int, private val winning: Set<Int>, private val numbers: Set<Int>) {
+        fun getMatchingNumber() = winning.intersect(numbers).size
     }
 }
--- aoc2023/test/org/carth/aoc2023/Day04Tests.kt
+++ aoc2023/test/org/carth/aoc2023/Day04Tests.kt
@@ -7,17 +7,17 @@ import kotlin.test.Test
 class Day04Tests : DayTests<Day04>(Day04::class) {
     @Test
     @Order(1)
-    fun solvePartOneSample() = solve(Part.ONE, Type.TEST, expected = 1)
+    fun solvePartOneSample() = solve(Part.ONE, Type.TEST, expected = 13)
 
     @Test
     @Order(2)
-    fun solvePartOne() = solve(Part.ONE, Type.INPUT, expected = 1)
+    fun solvePartOne() = solve(Part.ONE, Type.INPUT, expected = 26346)
 
     @Test
     @Order(3)
-    fun solvePartTwoSample() = solve(Part.TWO, Type.TEST, expected = 10)
+    fun solvePartTwoSample() = solve(Part.TWO, Type.TEST, expected = 30)
 
     @Test
     @Order(4)
-    fun solvePartTwo() = solve(Part.TWO, Type.INPUT, expected = 10)
+    fun solvePartTwo() = solve(Part.TWO, Type.INPUT, expected = 8467762)
 }
