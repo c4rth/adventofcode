cbc41a11f13e7c33812b21b358756bfc6fe6b78c
Thierry Carels <thierry.carels@gmail.com>
2023-12-15 07:33:01 +0100

    Day15

---
+++ aoc2023/test/org/carth/aoc2023/Day14Tests.kt
@@ -0,0 +1,24 @@
+package org.carth.aoc2023
+
+import org.carth.common.DayTests
+import org.carth.common.Part
+import org.junit.jupiter.api.Order
+import kotlin.test.Test
+
+class Day14Tests : DayTests<Day14>(Day14::class) {
+    @Test
+    @Order(10)
+    fun solvePartOneSample() = solve(Part.ONE, Type.TEST, expected = 136)
+
+    @Test
+    @Order(20)
+    fun solvePartOne() = solve(Part.ONE, Type.INPUT, expected = 109466)
+
+    @Test
+    @Order(30)
+    fun solvePartTwoSample() = solve(Part.TWO, Type.TEST, expected = 64)
+
+    @Test
+    @Order(40)
+    fun solvePartTwo() = solve(Part.TWO, Type.INPUT, expected = 94585)
+}
--- common/src/org/carth/common/Grids.kt
+++ common/src/org/carth/common/Grids.kt
@@ -32,3 +32,43 @@ class GridString(val lines: List<String>) {
     fun take(n: Int) = lines.take(n)
     fun drop(n: Int) = lines.drop(n)
 }
+
+@Suppress("unused")
+class MutableGridString(val lines: MutableList<CharArray>) {
+
+    constructor(input: String): this(input.lines().map { it.toCharArray() }.toMutableList())
+
+    val width = lines[0].size
+    val height = lines.size
+    val size = width to height
+
+    operator fun get(p: Point2d) = lines[p.y][p.x]
+    operator fun get(x: Int, y: Int) = lines[y][x]
+    operator fun get(y: Int) = lines[y]
+    operator fun set(p:Point2d, c: Char) { lines[p.y][p.x] = c }
+    operator fun set(x: Int, y: Int, c: Char) { lines[y][x] = c }
+
+    operator fun get(char : Char): Point2d? {
+        lines.forEachIndexed { y, line ->
+            line.forEachIndexed { x, c ->
+                if ( c == char) return Point2d(x,y)
+            }
+        }
+        return null
+    }
+
+    fun isInside(point: Point2d) = (point.x in 0 until width) && (point.y in 0 until height)
+
+    fun adjacent(x: Int, y: Int) = adjacent(Point2d(x,y))
+
+    fun adjacent(point: Point2d) = point.adjacent().filter { it.x >=0 && it.y >= 0 && it.x < width && it.y < height }
+
+    fun rotate() = GridString(List(lines.first().size) { i -> lines.joinToString("") { it[i].toString() } })
+
+    fun take(n: Int) = lines.take(n)
+    fun drop(n: Int) = lines.drop(n)
+
+    override fun toString() : String {
+        return lines.joinToString(System.lineSeparator()) { line -> line.joinToString("") }
+    }
+}
