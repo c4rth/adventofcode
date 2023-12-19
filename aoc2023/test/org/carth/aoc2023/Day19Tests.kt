c70a3116cb5391f0330a1fd2324047b79605c3e2
Thierry Carels <thierry.carels@gmail.com>
2023-12-19 21:06:27 +0100

    Day19

---
+++ aoc2023/test/org/carth/aoc2023/Day19Tests.kt
@@ -0,0 +1,24 @@
+package org.carth.aoc2023
+
+import org.carth.common.DayTests
+import org.carth.common.Part
+import org.junit.jupiter.api.Order
+import kotlin.test.Test
+
+class Day19Tests : DayTests<Day19>(Day19::class) {
+    @Test
+    @Order(10)
+    fun solvePartOneSample() = solve(Part.ONE, Type.TEST, expected = 19114L)
+
+    @Test
+    @Order(20)
+    fun solvePartOne() = solve(Part.ONE, Type.INPUT, expected = 406934L)
+
+    @Test
+    @Order(30)
+    fun solvePartTwoSample() = solve(Part.TWO, Type.TEST, expected = 167409079868000L)
+
+    @Test
+    @Order(40)
+    fun solvePartTwo() = solve(Part.TWO, Type.INPUT, expected = 131192538505367L)
+}
