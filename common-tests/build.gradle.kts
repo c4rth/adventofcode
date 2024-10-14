6d605de2fcd3c6511268f56c03726cba4b78f369
carth <me@carth.xyz>
2024-10-14 19:53:22 +0200

    upgrade gradle

--- common-tests/build.gradle.kts
+++ common-tests/build.gradle.kts
@@ -3,6 +3,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     api(platform("org.junit:junit-bom:5.10.1"))
     api("org.junit.jupiter:junit-jupiter")
+    runtimeOnly("org.junit.platform:junit-platform-launcher")
     api("org.jetbrains.kotlin:kotlin-test:2.0.21")
     api("org.junit.jupiter:junit-jupiter:5.10.1")
 }
